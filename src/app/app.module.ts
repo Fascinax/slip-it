f86b143e05708cccba2796387a19428e3a0716a8
Fascinax <tianason170@gmail.com>
2026-03-02 14:53:01 +0100

    feat: integrate ionicons and add icon declarations for improved UI consistency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,75 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { addIcons } from 'ionicons';
+import {
+  add,
+  alarmOutline,
+  arrowForwardCircle,
+  chatbubbles,
+  checkmarkCircle,
+  closeCircle,
+  diceOutline,
+  eye,
+  eyeOff,
+  flash,
+  gameController,
+  infinite,
+  keyOutline,
+  linkOutline,
+  locateOutline,
+  medalOutline,
+  people,
+  peopleCircle,
+  personAdd,
+  phonePortraitOutline,
+  playCircle,
+  refresh,
+  shareSocial,
+  statsChartOutline,
+  time,
+  timeOutline,
+  trash,
+  trashOutline,
+  trophy,
+  trophyOutline,
+} from 'ionicons/icons';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+addIcons({
+  add,
+  'alarm-outline': alarmOutline,
+  'arrow-forward-circle': arrowForwardCircle,
+  chatbubbles,
+  'checkmark-circle': checkmarkCircle,
+  'close-circle': closeCircle,
+  'dice-outline': diceOutline,
+  eye,
+  'eye-off': eyeOff,
+  flash,
+  'game-controller': gameController,
+  infinite,
+  'key-outline': keyOutline,
+  'link-outline': linkOutline,
+  'locate-outline': locateOutline,
+  'medal-outline': medalOutline,
+  people,
+  'people-circle': peopleCircle,
+  'person-add': personAdd,
+  'phone-portrait-outline': phonePortraitOutline,
+  'play-circle': playCircle,
+  refresh,
+  'share-social': shareSocial,
+  'stats-chart-outline': statsChartOutline,
+  time,
+  'time-outline': timeOutline,
+  trash,
+  'trash-outline': trashOutline,
+  trophy,
+  'trophy-outline': trophyOutline,
+});
 
 @NgModule({
   declarations: [AppComponent],
